841bdf0a066f0b2bc5c6fdca5d650b58618944e6
ArpitTripathi945 <ad3617@srmist.edu.in>
2024-08-02 22:21:17 +0530

    Updates

---
+++ precache-manifest.d661d3e00819e79bfd9b4664a968b3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d362a5dc2f8444be8f4a081585337b14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc5a7f8b1cdc47b73b5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e41da8d48ff2c4cbadad",
+    "url": "/static/js/2.31a66e6b.chunk.js"
+  },
+  {
+    "revision": "0a0b602d25062375dacd",
+    "url": "/static/js/3.40a56a06.chunk.js"
+  },
+  {
+    "revision": "0bc5a7f8b1cdc47b73b5",
+    "url": "/static/js/main.72824fef.chunk.js"
+  },
+  {
+    "revision": "42fa70b8bfc53216255b",
+    "url": "/static/js/runtime~main.5379b062.js"
+  }
+]);
